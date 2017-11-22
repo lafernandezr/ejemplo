77b2b2c2e91b4e104c84dcd61b69268ba2f567ac
lafernandezr <lafernandezr@indra.es>
2017-11-22 13:08:18 +0100

    añadido prueba

---
+++ prueba/src/main/java/com/mycompany/prueba/NewClass.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.prueba;
+    
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author formacion
+ */
+public class NewClass {
+    private Integer x;
+
+    public Integer getX() {
+        
+        return x;
+    }
+    private static final Logger LOG = Logger.getLogger(NewClass.class.getName());
+
+    public void setX(Integer x) {
+        this.x = x;
+    }
+    
+    
+    
+}
